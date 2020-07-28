4523d0566b70ad981e853b829bd485679aaed3f4
SajjadBrohi <s.brohi@uq.net.au>
2020-07-29 00:22:46 +1000

    To-do list completed

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+const toDoActivities = [];
+const workActivities = [];
 
 app.get('/', (req, res) => {
 	const daysArray = [
@@ -18,7 +23,35 @@ app.get('/', (req, res) => {
 	const date = new Date();
 	const day = daysArray[date.getDay()];
 
-	res.render('index', { currentDay: day });
+	res.render('index', {
+		currentDay: day,
+		toDoActivity: toDoActivities,
+		toDoType: 'home',
+	});
+	// Compiler reaches here first, sees that there
+	// is no variable so gives error. So you will have
+	// to define your variables here in order to not
+	// get an error
+});
+
+app.get('/work', (req, res) => {
+	res.render('index', {
+		currentDay: 'Work',
+		toDoActivity: workActivities,
+		toDoType: 'work',
+	});
+});
+
+app.post('/', (req, res) => {
+	const toDoActivity = req.body.todo;
+
+	if (req.body.button === 'home') {
+		toDoActivities.push(toDoActivity);
+		res.redirect('/');
+	} else {
+		workActivities.push(toDoActivity);
+		res.redirect('/work');
+	}
 });
 
 app.listen(3000, () => console.log('Server started on port 3000.'));

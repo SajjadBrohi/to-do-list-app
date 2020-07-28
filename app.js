e736800cd55b667dca24196fa753e39a4b2526c4
SajjadBrohi <s.brohi@uq.net.au>
2020-07-28 19:52:05 +1000

    EJS added for day calculation

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-	res.send('You made it!');
+	const daysArray = [
+		'Sunday',
+		'Monday',
+		'Tuesday',
+		'Wednesday',
+		'Thursday',
+		'Friday',
+		'Saturday',
+		'Sunday',
+	];
+
+	const date = new Date();
+	const day = daysArray[date.getDay()];
+
+	res.render('index', { currentDay: day });
 });
 
 app.listen(3000, () => console.log('Server started on port 3000.'));
